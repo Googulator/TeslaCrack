6a0ad34da170233797eed4c02bdab723a96fafe3
Googulator <netrolller.3d@gmail.com>
2015-12-29 02:28:21 +0100

    Workaround for short keys
    Handle a quirk of TeslaCrypt's AES key scheduler, where keys that happen
    to begin with NUL bytes have the NULs moved to the end instead.

--- unfactor.py
+++ unfactor.py
@@ -2,6 +2,11 @@ from Crypto.Cipher import AES
 import binascii
 import sys
 
+def fix_key(key):
+    while key[0] == '\0':
+        key = key[1:] + '\0'
+    return key
+
 def main(args, magic = '%PDF', short_key_limit = 240):
     if len(args) < 2:
         print "usage: unfactor.py <sample file> <space-separated list of factors>"
@@ -21,7 +26,7 @@ def main(args, magic = '%PDF', short_key_limit = 240):
             for j in xrange(len(primes)):
                 if i & 1<<j:
                     x *= int(primes[j])
-            if 1<<short_key_limit < x < 1<<256 and AES.new(binascii.unhexlify('%064x' % x), AES.MODE_CBC, header[0x18a:0x19a]).decrypt(data).startswith(magic):
+            if 1<<short_key_limit < x < 1<<256 and AES.new(fix_key(binascii.unhexlify('%064x' % x)), AES.MODE_CBC, header[0x18a:0x19a]).decrypt(data).startswith(magic):
                 print "Candidate AES private key: b'\\x" + '\\x'.join([('%064x' % x)[i:i+2] for i in xrange(0, 64, 2)]) + "' (%064X)" % x
             
 if __name__ == '__main__':
