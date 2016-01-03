881211dfc0b154a1c82192d322d84749d77902c2
Googulator <netrolller.3d@gmail.com>
2016-01-03 05:49:59 +0100

    Add Bitcoin-based key reconstructor for ancient TeslaCrypt (ecc/exx/ezz files)

---
+++ unfactor-bitcoin.py
@@ -0,0 +1,46 @@
+# Bitcoin address-based TeslaCrypt key reconstructor
+#
+# This is an alternative to unfactor-ecdsa.py, which should also work with
+# ancient versions of TeslaCrypt.
+# 
+# To use this tool, you need the Bitcoin address where ransom was expected to be paid,
+# as well as the 512-bit Bitcoin shared secret. This is typically found in the recovery
+# file, which is a text file named "RECOVERY_KEY.TXT", "recover_file.txt", or similar
+# dropped in the Documents folder by TeslaCrypt.
+# The first line of the recovery file is the Bitcoin address, while the 3rd line is
+# the shared secret. These values can also be obtained from key.dat, storage.bin
+# TeslaCrypt's registry entry, or (in case of TeslaCrypt 2.x) from the encrypted files
+# or from network packet dumps, in case the recovery file is lost.
+#
+# Once you have these values, factor the shared secrets, then run this script with the
+# factors, like this:
+# unfactor-bitcoin.py <1st line of recovery file> <factors of 3rd line of recovery file>
+# The generated key can then be used with TeslaDecoder.
+
+from coinkit.keypair import BitcoinKeypair
+import sys
+
+def main(args, short_key_limit = 240):
+    if len(args) <= 2:
+        print "usage: unfactor-bitcoin.py <bitcoin address> <space-separated list of factors>"
+
+    primes = args[1:]
+
+    addrs = {}
+    
+    for i in xrange(1<<len(primes)):
+        x = 1
+        for j in xrange(len(primes)):
+            if i & 1<<j:
+                x *= int(primes[j])
+        if 1<<short_key_limit < x < 1<<256:
+            if x not in addrs:
+                addrs[x] = BitcoinKeypair(x).address()
+            if args[0] == addrs[x]:
+                print "Found Bitcoin private key: %064X" % x
+                return
+
+    print "No keys found, check your factors!"
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
