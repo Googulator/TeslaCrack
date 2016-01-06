cc4ea1e35e4ecb495fd920e104255cb81a045a03
Googulator <netrolller.3d@gmail.com>
2016-01-06 01:25:11 +0100

    Support Python 3

--- unfactor.py
+++ unfactor.py
@@ -4,14 +4,19 @@ import binascii
 import sys
 
 def fix_key(key):
-    while key[0] == '\0':
-        key = key[1:] + '\0'
+    while key[0] == b'\0':
+        key = key[1:] + b'\0'
     return key
 
-def main(file, primes, magic = '%PDF'):
-    known_file_magics = ['\xde\xad\xbe\xef\x04', '\x00\x00\x00\x00\x04']
+def main(file, primes, magic = b'%PDF'):
+    known_file_magics = [b'\xde\xad\xbe\xef\x04', b'\x00\x00\x00\x00\x04']
     ret = ""
-    
+
+    try:
+        xrange
+    except NameError:
+        xrange = range
+
     prod = 1
     for i in xrange(len(primes)):
         prod *= int(primes[i])
@@ -20,8 +25,8 @@ def main(file, primes, magic = '%PDF'):
         header = f.read(414)
         if header[:5] not in known_file_magics:
             return file + " doesn't appear to be TeslaCrypted"
-        ecdh = int(header[0x108:0x188].rstrip('\0'), 16)
-        cofactor = ecdh/prod
+        ecdh = int(header[0x108:0x188].rstrip(b'\0'), 16)
+        cofactor = ecdh//prod
         if prod > ecdh:
             return "Superfluous factors or incorrect factorization detected!"
         if cofactor*prod != ecdh:
@@ -37,7 +42,7 @@ def main(file, primes, magic = '%PDF'):
             for j in xrange(len(primes)):
                 if i & 1<<j:
                     x *= int(primes[j])
-            if x < 1<<256 and ecdh/x < 1<<256 and AES.new(fix_key(binascii.unhexlify('%064x' % x)), AES.MODE_CBC, header[0x18a:0x19a]).decrypt(data).startswith(magic):
+            if x < 1<<256 and ecdh//x < 1<<256 and AES.new(fix_key(binascii.unhexlify('%064x' % x)), AES.MODE_CBC, header[0x18a:0x19a]).decrypt(data).startswith(magic):
                 ret += "Candidate AES private key: b'\\x" + '\\x'.join([('%064x' % x)[i:i+2] for i in xrange(0, 64, 2)]) + ("' (%064X)" % x) + "\n"
                 found = True
             i += 1
@@ -48,7 +53,7 @@ def main(file, primes, magic = '%PDF'):
                 for j in xrange(len(primes)):
                     if i & 1<<j:
                         x *= int(primes[j])
-                if x < 1<<256 and ecdh/x < 1<<256 and AES.new(fix_key(binascii.unhexlify('%064x' % x)), AES.MODE_CBC, header[0x18a:0x19a]).decrypt(data).startswith(magic):
+                if x < 1<<256 and ecdh//x < 1<<256 and AES.new(fix_key(binascii.unhexlify('%064x' % x)), AES.MODE_CBC, header[0x18a:0x19a]).decrypt(data).startswith(magic):
                     ret += "Candidate AES private key: b'\\x" + '\\x'.join([('%064x' % x)[i:i+2] for i in xrange(0, 64, 2)]) + ("' (%064X)" % x) + "\n"
                 i += 1
     
@@ -58,4 +63,4 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("usage: unfactor.py <sample file> <space-separated list of factors>")
         exit()
-    print(main(sys.argv[1], sys.argv[2:], "\xff\xd8"))
+    print(main(sys.argv[1], sys.argv[2:]))
